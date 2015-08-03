f184abaa9f3191c45d64c0a4445a202675eb9ef4
raulghm <raulghm@gmail.com>
2015-08-03 02:03:41 -0300

    updating

--- src/scripts/functions.js
+++
@@ -1,9 +0,0 @@
-var sectionAutoHeight = function() {
-	if ($(window).width() > 768) {
-		return $('.u-autoHeight').height($(window).height());
-	} else {
-		return $('.u-autoHeight').css({
-			height: "inherit"
-		});
-	}
-};
--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -4,43 +4,51 @@
  * Main scripts
  */
 
-$(window).resize(function() {
-	sectionAutoHeight();
-})
+/* global $ */
 
-$(window).scroll(function() {
-  var div, opa, scrollPosition;
+const $ = 'jQuery';
 
-  scrollPosition = $(window).scrollTop();
-
-  if ($(window).width() > 768) {
-    // parallax video
-    // $('video').css('transform', 'translateY(' + scrollPosition * 0.2 + 'px)');
-    // opa = 1 - (scrollPosition / 1000);
-    // $('video').css({'opacity': opa});
-
-    // parallax bg hero
-    // $('.img-hero-1').css('transform', 'translateY(' + scrollPosition * 0.2 + 'px)');
-    $('.img-hero-2').css('transform', 'translateY(-' + scrollPosition * 0.1 + 'px)');
-    $('.img-hero-3').css('transform', 'translateY(-' + scrollPosition * 0.2 + 'px)');
-  }
+$(window).resize( () => {
+	// sectionAutoHeight();
 });
 
-$(document).ready(function() {
-	setTimeout(function() {
-		return $("html").addClass("init");
+/*
+$(window).scroll( () => {
+	let div;
+	let opa;
+	// const scrollPosition = '';
+
+	const scrollPosition = $(window).scrollTop();
+
+	if ($(window).width() > 768) {
+		// parallax video
+		// $('video').css('transform', 'translateY(' + scrollPosition * 0.2 + 'px)');
+		// opa = 1 - (scrollPosition / 1000);
+		// $('video').css({'opacity': opa});
+
+		// parallax bg hero
+		// $('.img-hero-1').css('transform', 'translateY(' + scrollPosition * 0.2 + 'px)');
+		$('.img-hero-2').css('transform', 'translateY(-' + scrollPosition * 0.1 + 'px)');
+		$('.img-hero-3').css('transform', 'translateY(-' + scrollPosition * 0.2 + 'px)');
+	}
+});
+*/
+
+$(document).ready( () => {
+	setTimeout(() => {
+		return $('html').addClass('init');
 	}, 10);
 
-	setTimeout(function() {
-		return $("html").addClass("init-1");
+	setTimeout(() => {
+		return $('html').addClass('init-1');
 	}, 700);
 
-	sectionAutoHeight();
+	// sectionAutoHeight();
 
-	$('.Team-item').on('click', function(){
-		var github = $(this).data('github');
+	$('.Team-item').on('click', () => {
+		const github = $(this).data('github');
 		if (github) {
 			window.open('https://github.com/' + github, '_blank');
 		}
-	})
+	});
 });
